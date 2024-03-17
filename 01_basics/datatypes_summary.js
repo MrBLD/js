2e80766624c5417c463894a77ce4b99e711cbc2b
Bhaskar Lal Das <120587599+MrBLD@users.noreply.github.com>
2024-03-17 18:00:40 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -31,9 +31,32 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId, anotherId);
-console.log(typeof bigNumber);
-console.log(typeof heros);
-console.log(typeof anotherId);
+console.log(1, typeof anotherId, anotherId);
+console.log(2, typeof bigNumber);
+console.log(3, typeof heroes);
+console.log(4, typeof myobj);
+console.log(5, typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// Stack(Primitive//copy), Heap (Non-Primitive)
+
+let King="Ben"
+let who=King
+who="Mr.BLD"
+
+// in the heap // used by reference
+let Kingdom={
+    ruler:King,
+    name:"Ben",
+    arm:"orion_arm",
+    galaxy:"Milky_way",
+    supercluster:"Virgo"
+}
+
+let Kingdom2=Kingdom
+Kingdom2.name="Bhaskar"
+
+console.log(Kingdom.name);
+console.log(Kingdom2.name);
