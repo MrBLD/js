0698cc44ac423e2d1e8c62e2b4312e3da9e6cd7d
Bhaskar Lal Das <120587599+MrBLD@users.noreply.github.com>
2024-03-17 17:37:13 +0000

    rearrange

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,14 +8,19 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
 
+// avoid these type of comparisions
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+/* 
+the reason for not predictable result is equality == and comparisions > < >= <= work differently
+comparisions convert nusll t a number, treating it as 0
+*/
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// === // checks datatype also| doesnt do conversions
 
 console.log("2" === 2);
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+//  Primitive // default call by value
+
+//  7 types : String, Number, Boolearn, null(empty), undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+
+// Reference (Non primitive) // def pass by reference
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"]; // object
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId, anotherId);
+console.log(typeof bigNumber);
+console.log(typeof heros);
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
