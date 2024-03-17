1791bf3f2bad1840003953a5aadd67e715c1df92
Bhaskar Lal Das <120587599+MrBLD@users.noreply.github.com>
2024-03-17 17:06:27 +0000

    conversion

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -42,19 +42,20 @@ let negValue = -value
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " king"
 
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+// if string on first then as string if at last then the operation first
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
+// console.log(+true); // converted to number
 // console.log(+"");
 
 let num1, num2, num3
