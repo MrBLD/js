78a077d198b37582c2e74b8c9f0b22b73f7f32a7
Bhaskar Lal Das <120587599+MrBLD@users.noreply.github.com>
2024-03-18 17:37:08 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -14,6 +14,9 @@ console.log("Global:", a);
 // console.log(b);
 console.log(c);
 
+
+// global scope is different in windows and in node::DOM
+// small can take ice cream from big but big cant from small
 function one() {
   const username = "hitesh";
 
@@ -47,6 +50,9 @@ function addone(num) {
   return num + 1;
 }
 
+
+// hoisting
+//  in addtwo, addtwo is declared and holded in a variable
 const addTwo = function (num) {
   return num + 2;
 };
